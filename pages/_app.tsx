cb02280b3539f33d5e943806310ef4c7b9550895
ryanrw <surasak@opn.ooo>
2022-12-31 18:36:51 +0700

    feat(theme): add custom theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        width: "100%",
+        height: "100vh",
+        bgGradient: "linear(to-b, #2D2424, #5C3D2E)",
+        color: "#FFF",
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
