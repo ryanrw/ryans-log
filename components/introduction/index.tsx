6cf77f8c4d18a6ee8ad79b4e63e24215b47e8122
ryanrw <surasak@opn.ooo>
2022-12-31 18:55:30 +0700

    feat: add introduction components

---
+++ components/introduction/index.tsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Avatar,
+  Flex,
+  Box,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+
+export const Introduction = () => {
+  return (
+    <Card bg="#221b1b" color="#fff" p={4}>
+      <Flex alignItems="center">
+        <Avatar width="60" height="60" src="/avatar/ryan.jpg" />
+
+        <Box ml={4}>
+          <Heading mb={2}>Hello, สวัสดีครับ! 👋 I'm Ryan!</Heading>
+          <Text fontSize="2xl">
+            This is Ryan's story log where every miserable memory will be logged
+            here!
+          </Text>
+          <Text fontSize="xl">It's nice to see you here 🧑🏽‍💻</Text>
+        </Box>
+      </Flex>
+    </Card>
+  );
+};
