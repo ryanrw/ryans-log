065449b6e91932a5dffa7d9dc74161058d19a054
ryanrw <surasak@opn.ooo>
2022-12-31 20:11:34 +0700

    feat: add post page

--- components/post-list/index.tsx
+++ components/post-list/index.tsx
@@ -8,6 +8,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import type { PostsOrPages } from "@tryghost/content-api";
+import Link from "next/link";
 
 interface PostListProps {
   posts: PostsOrPages;
@@ -17,18 +18,20 @@ export const PostList: React.FC<PostListProps> = ({ posts }) => {
   return (
     <SimpleGrid mt={12} columns={1} spacing="20px">
       {posts.map((post) => (
-        <Card bg="#221b1b" color="#fff" p={4} key={post.id} variant="coffee">
-          <CardBody>
-            <Image
-              src={post.feature_image || ""}
-              alt={post.feature_image_alt || "content image"}
-            />
-          </CardBody>
-          <Stack mt="6" spacing="3">
-            <Heading size="md">{post.title}</Heading>
-            <Text>{post.excerpt}</Text>
-          </Stack>
-        </Card>
+        <Link href={`/log/${post.slug}`}>
+          <Card bg="#221b1b" color="#fff" p={4} key={post.id} variant="coffee">
+            <CardBody>
+              <Image
+                src={post.feature_image || ""}
+                alt={post.feature_image_alt || "content image"}
+              />
+            </CardBody>
+            <Stack mt="6" spacing="3">
+              <Heading size="md">{post.title}</Heading>
+              <Text>{post.excerpt}</Text>
+            </Stack>
+          </Card>
+        </Link>
       ))}
     </SimpleGrid>
   );
