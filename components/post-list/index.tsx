18c82bb80f5e67190092a895510700a86eebd10b
ryanrw <surasak@opn.ooo>
2022-12-31 19:42:29 +0700

    feat: add post list at index page

---
+++ components/post-list/index.tsx
@@ -0,0 +1,35 @@
+import {
+  Card,
+  SimpleGrid,
+  Image,
+  CardBody,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import type { PostsOrPages } from "@tryghost/content-api";
+
+interface PostListProps {
+  posts: PostsOrPages;
+}
+
+export const PostList: React.FC<PostListProps> = ({ posts }) => {
+  return (
+    <SimpleGrid mt={12} columns={2} spacing="20px">
+      {posts.map((post) => (
+        <Card bg="#221b1b" color="#fff" p={4} key={post.id} variant="coffee">
+          <CardBody>
+            <Image
+              src={post.feature_image || ""}
+              alt={post.feature_image_alt || "content image"}
+            />
+          </CardBody>
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{post.title}</Heading>
+            <Text>{post.excerpt}</Text>
+          </Stack>
+        </Card>
+      ))}
+    </SimpleGrid>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import { GetStaticProps } from "next";
+import { PostsOrPages } from "@tryghost/content-api";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { Introduction } from "../components/introduction";
 import { Layout } from "../components/layout";
+import { PostList } from "../components/post-list";
 import { getAllPost } from "../utils/content-api";
 
-export const getStaticProps: GetStaticProps = async (context) => {
+interface HomeProps {
+  posts: PostsOrPages;
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async (_context) => {
   const posts = await getAllPost();
 
   console.log(posts);
@@ -15,11 +21,15 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
-export default function Home() {
+export default function Home({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Layout>
         <Introduction />
+
+        <PostList posts={posts} />
       </Layout>
     </>
   );
